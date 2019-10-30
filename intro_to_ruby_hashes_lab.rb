9b10ea6c62d488f7e055c883b3e2fae3ec3091f0
Juan Gonzalez <3232rose@gmail.com>
2019-10-30 01:17:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,12 +16,12 @@ end
 
 def id_generator
   id_hash = {
-    id: 23
+    id: 26
   }
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  
 end
 
 def read_from_hash(hash, key)
